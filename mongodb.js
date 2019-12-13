920e3436ddbaaa4933b04046d4f561efd95df53f
Роман Ковальчук <romiko@Romans-MacBook-Pro.local>
2019-12-13 08:06:18 +0300

    Init git repo

---
+++ mongodb.js
@@ -0,0 +1,16 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-app';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+
+
+    
+});
---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (value === 'password') {
+                throw new Error('Password cant be equal password');
+            }
+            if (value.length < 7) {
+                throw new Error('Password length must be greater than 6');
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive nymber');
+            }
+        }
+    }
+});
+
+module.exports = User;
